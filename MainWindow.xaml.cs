db9bd8e6413dfc963e8ab34e3e7b525da40fb3ee
D-Vnuli <d.stronsky.ai@gmail.com>
2026-07-11 11:54:45 +0300

    работа с визуальным интерфейсом

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,38 @@
-﻿using System.Windows;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using CosmoNet.App.ViewModels;
 
 namespace CosmoNet.App;
 
-public partial class MainWindow : Window
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
     private readonly MainViewModel _viewModel = new();
+    private bool _isMenuOpen;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public ICommand ToggleMenuCommand { get; }
+
+    public bool IsMenuOpen
+    {
+        get => _isMenuOpen;
+        private set
+        {
+            if (_isMenuOpen == value)
+            {
+                return;
+            }
+
+            _isMenuOpen = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsMenuOpen)));
+        }
+    }
 
     public MainWindow()
     {
+        ToggleMenuCommand = new RelayCommand(ToggleMenuAsync);
         InitializeComponent();
         DataContext = _viewModel;
         Loaded += OnLoaded;
@@ -18,5 +42,30 @@ public partial class MainWindow : Window
     private async void OnLoaded(object sender, RoutedEventArgs e)
     {
         await _viewModel.InitializeAsync();
+    }
+
+    private Task ToggleMenuAsync()
+    {
+        IsMenuOpen = !IsMenuOpen;
+        return Task.CompletedTask;
+    }
+
+    private void OnMenuBackdropMouseDown(object sender, MouseButtonEventArgs e)
+    {
+        IsMenuOpen = false;
+    }
+
+    private void OnMenuSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (sender is ListBox menu && menu.SelectedIndex >= 0)
+        {
+            MainViews.SelectedIndex = menu.SelectedIndex;
+            IsMenuOpen = false;
+        }
+    }
+
+    private void OnMenuNavigationMouseUp(object sender, MouseButtonEventArgs e)
+    {
+        IsMenuOpen = false;
     }
 }
